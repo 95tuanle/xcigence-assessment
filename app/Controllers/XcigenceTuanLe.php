96d01d9e825d393db51fb83a1435d86d975b53e2
Tuan Le <tlna95@icloud.com>
2023-07-28 02:17:56 -0400

    taste test the water

---
+++ app/Controllers/XcigenceTuanLe.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class XcigenceTuanLe extends BaseController
+{
+    public function index(): string
+    {
+        return view('xcigence_tuan_le');
+    }
+}
