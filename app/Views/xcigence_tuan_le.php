ed5f32c5485d593a4ff0e55cd083e40cfa0f2563
Tuan Le <tlna95@icloud.com>
2023-07-31 14:17:04 -0400

    lock down the logic

--- app/Views/xcigence_tuan_le.php
+++ app/Views/xcigence_tuan_le.php
@@ -10,40 +10,30 @@
             height: 100%;
             margin: 0;
         }
-
         .sidebar {
-            background-color: #007BFF; /* Blue color */
+            background-color: #0D1B61;
             color: #fff;
             padding: 20px;
             box-sizing: border-box;
         }
-
         .content {
             padding: 20px;
-            background-color: #f0f0f0; /* Light gray color for content area */
-            flex: 1; /* Occupy remaining height */
-            min-height: 0; /* Reset the default min-height property */
+            background-color: #f0f0f0;
+            flex: 1;
+            min-height: 0;
         }
-
-        /* Optional: Style the pre element to make the JSON content scrollable if it overflows */
         pre {
             overflow: auto;
             max-height: 100%;
         }
-
     </style>
 </head>
 <body>
 <div class="d-flex h-100">
-    <!-- Sidebar -->
     <div class="col-md-3 col-lg-2 sidebar">
-        <!-- Add your sidebar content here -->
-        Sidebar Content
+        <h1>Xcigence</h1>
     </div>
-    <!-- Main Content -->
-    <div class="col-md-9 col-lg-10 d-flex flex-column content">
-
-    </div>
+    <div class="col-md-9 col-lg-10 d-flex flex-column content"></div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -53,10 +43,41 @@
     } else {echo null;}  ?>;
     if (reportData !== null) {
         console.log(reportData);
-        const container = document.querySelector('.content');
+        const content = document.querySelector('.content');
         const pre = document.createElement('pre');
         pre.innerText = JSON.stringify(reportData, null, 2);
-        container.appendChild(pre);
+        content.appendChild(pre);
+
+        const sidebar = document.querySelector('.sidebar');
+
+        const mostRecentChildKeys = getMostRecentChildKeys(reportData);
+
+        function getMostRecentChildKeys(data) {
+            let mostRecentChildren = [];
+            if (Array.isArray(data)) {
+                mostRecentChildren = data.map(item => item.name);
+            } else if (typeof data === 'object' && data !== null) {
+                mostRecentChildren = Object.keys(data)
+            }
+            return mostRecentChildren;
+        }
+
+        mostRecentChildKeys.forEach(key => {
+            const button = document.createElement('button');
+            button.className = 'btn btn-primary btn-block';
+            button.innerText = formatKeyName(key);
+            button.onclick = () => showData(key);
+            sidebar.appendChild(button);
+        });
+
+        function formatKeyName(key) {
+            const words = key.split('_');
+            return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        }
+
+        function showData(key) {
+            console.log(`Show data for ${key}`);
+        }
     } else {
         alert('No data found!');
     }
