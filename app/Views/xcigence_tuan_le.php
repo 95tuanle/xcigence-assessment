26484aea552e0a5fe0fab85a244f9195f408755f
Tuan Le <tlna95@icloud.com>
2023-07-31 16:36:48 -0400

    finish with the report detail

--- app/Views/xcigence_tuan_le.php
+++ app/Views/xcigence_tuan_le.php
@@ -20,7 +20,7 @@
             color: #192462;
         }
         pre {
-            overflow: auto;
+            white-space: pre-wrap;
             max-height: 100%;
         }
         .sidebar {
@@ -57,8 +57,7 @@
     </div>
     <div class="col-md-9 col-lg-10 d-flex flex-column content">
         <h2 id="header" class="header"></h2>
-        <pre>
-        </pre>
+        <pre></pre>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
@@ -114,7 +113,81 @@
             const header = document.getElementById('header');
             header.innerText = headerContent;
             const content = document.querySelector('.content pre');
-            content.innerText = JSON.stringify(dataContent, null, 2);
+            switch (headerContent) {
+                case 'Home':
+                    content.innerHTML = JSON.stringify(dataContent, null, 2);
+                    break;
+                case 'Report Detail':
+                    content.innerHTML = reportDetailContent(dataContent);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        function reportDetailContent(dataContent) {
+            return `<div>
+                        <table class="table table-bordered">
+                            <tr>
+                                <td><strong>Client ID:</strong></td>
+                                <td>${dataContent["clientid"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Scan ID:</strong></td>
+                                <td>${dataContent["scan_id"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Client Name:</strong></td>
+                                <td>${dataContent["client_name"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Client Email:</strong></td>
+                                <td>${dataContent["client_email"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Client Industry:</strong></td>
+                                <td>${dataContent["client_industry"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Client Web/IP:</strong></td>
+                                <td><a href="${dataContent["client_web_ip"]}">${dataContent["client_web_ip"]}</a></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Final Score:</strong></td>
+                                <td>${dataContent["final_score"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Status:</strong></td>
+                                <td>${dataContent["status"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Low Vulnerabilities:</strong></td>
+                                <td>${dataContent["Low_vuln"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Medium Vulnerabilities:</strong></td>
+                                <td>${dataContent["Medium_vuln"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>High Vulnerabilities:</strong></td>
+                                <td>${dataContent["High_vuln"]}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Critical Vulnerabilities:</strong></td>
+                                <td>${dataContent["Critical_vuln"]}</td>
+                            </tr>
+                        </table>
+                        <h4>Vulnerability Details</h4>
+                        <ul>
+                            <li><strong>Alert:</strong> ${dataContent["Vulnerability"][0].alert}</li>
+                            <li><strong>CVE:</strong> ${dataContent["Vulnerability"][0]["CVE"]}</li>
+                            <li><strong>Severity:</strong> ${dataContent["Vulnerability"][0]["severity"]}</li>
+                            <li><strong>URI:</strong> <a href="${dataContent["Vulnerability"][0]["uri"]}">${dataContent["Vulnerability"][0]["uri"]}</a></li>
+                            <li><strong>Description:</strong> ${dataContent["Vulnerability"][0]["description"]}</li>
+                            <li><strong>Solution:</strong> ${dataContent["Vulnerability"][0]["solution"]}</li>
+                        </ul>
+                    </div>`;
         }
 
         function clearButtonsStyle() {
