b56a7b6330c021f89139638e98ca11c1117e8b9a
Tuan Le <tlna95@icloud.com>
2023-08-01 02:46:35 -0400

    restructure

--- app/Views/xcigence_tuan_le.php
+++ app/Views/xcigence_tuan_le.php
@@ -56,7 +56,9 @@
 <body>
 <div class="d-flex h-100">
     <div class="col-md-3 col-lg-2 sidebar">
-        <h1 onclick="showHome()">Xcigence</h1>
+        <h1 class="home">Xcigence</h1>
+        <div class="sidebar-buttons"></div>
+        <div class="signature"></div>
     </div>
     <div class="col-md-9 col-lg-10 d-flex flex-column content">
         <h2 id="header" class="header"></h2>
@@ -65,77 +67,59 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script>
-    const reportData = <?php if (isset($report)) {echo json_encode($report);} else {echo null;}?>;
-    if (reportData !== null) {
-        showHome();
-        const sidebar = document.querySelector('.sidebar');
-        let mostRecentChildKeys = [];
-        if (Array.isArray(reportData)) {
-            mostRecentChildKeys = reportData.map(item => item.name);
-        } else if (typeof reportData === 'object') {
-            mostRecentChildKeys = Object.keys(reportData)
-        }
-        mostRecentChildKeys.forEach(key => {
-            const button = document.createElement('button');
-            button.className = 'btn btn-primary btn-block';
-            button.innerText = formatKeyName(key);
-            button.onclick = (event) => showBasedOnChildClicked(event, key);
-            sidebar.appendChild(button);
-        });
-        const signature = document.createElement('div');
-        signature.className = 'signature';
-        signature.innerText = new Date().getFullYear().toString() + ' Nguyen Anh Tuan Le';
-        sidebar.appendChild(signature);
-
-        function formatKeyName(key) {
-            const words = key.split('_');
-            return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-        }
-
-        function showHome() {
-            console.log(reportData);
-            constructContent('Home', reportData)
-        }
-
-        function showBasedOnChildClicked(event, key) {
-            console.log(reportData[key]);
-            constructContent(formatKeyName(key), reportData[key]);
-            event.target.classList.add('active');
-        }
-
-        function constructContent(headerContent, dataContent) {
-            const buttons = document.querySelectorAll('.sidebar button.btn');
-            buttons.forEach(button => button.classList.remove('active'));
-            const header = document.getElementById('header');
-            header.innerText = headerContent;
-            const content = document.querySelector('.content pre');
-            switch (headerContent) {
-                case 'Home':
-                    content.innerHTML = JSON.stringify(dataContent, null, 2);
-                    break;
-                case 'Report Detail':
-                    content.innerHTML = reportDetailContent(dataContent);
-                    break;
-                case 'Threatened':
-                    content.innerHTML = threatenedContent(dataContent);
-                    break;
-                case 'Digital User Risk':
-                    content.innerHTML = digitalUserRiskContent(dataContent);
-                    break;
-                default:
-                    break;
+    document.addEventListener('DOMContentLoaded', () => {
+        const reportData = <?php if (isset($report)) {echo json_encode($report);} else {echo null;}?>;
+        if (reportData !== null) {
+            function formatKeyName(key) {
+                return key.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
             }
-        }
 
-        function threatenedContent(dataContent) {
-            if (Array.isArray(dataContent) && dataContent.length > 0) {
-                let contentHTML = '<ul>';
-                dataContent.forEach((threatenedItem, index) => {
-                    contentHTML += `<li><strong>Threat ${index + 1}:</strong><br>`;
-                    contentHTML += '<table class="table table-bordered">';
-                    for (const [key, value] of Object.entries(threatenedItem)) {
-                        if (Array.isArray(value)) {
-                            contentHTML += `<tr>
+            function showHome() {
+                const buttons = document.querySelectorAll('.sidebar button.btn');
+                buttons.forEach(button => button.classList.remove('active'));
+                constructContent('Home', reportData);
+            }
+
+            function showBasedOnChildClicked(event, key) {
+                constructContent(formatKeyName(key), reportData[key]);
+                const activeButton = document.querySelector('.sidebar .btn.active');
+                if (activeButton) {
+                    activeButton.classList.remove('active');
+                }
+                event.target.classList.add('active');
+            }
+
+            function constructContent(headerContent, dataContent) {
+                const header = document.getElementById('header');
+                header.innerText = headerContent;
+                const content = document.querySelector('.content pre');
+                switch (headerContent) {
+                    case 'Home':
+                        content.innerHTML = JSON.stringify(dataContent, null, 2);
+                        break;
+                    case 'Report Detail':
+                        content.innerHTML = reportDetailContent(dataContent);
+                        break;
+                    case 'Threatened':
+                        content.innerHTML = threatenedContent(dataContent);
+                        break;
+                    case 'Digital User Risk':
+                        content.innerHTML = digitalUserRiskContent(dataContent);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            function threatenedContent(dataContent) {
+                if (Array.isArray(dataContent) && dataContent.length > 0) {
+                    let contentHTML = '<ul>';
+                    dataContent.forEach((threatenedItem, index) => {
+                        contentHTML += `<li><strong>Threat ${index + 1}:</strong><br>`;
+                        contentHTML += '<table class="table table-bordered">';
+                        for (const [key, value] of Object.entries(threatenedItem)) {
+                            if (Array.isArray(value)) {
+                                contentHTML += `<tr>
                                                 <td><strong>${formatKeyName(key)}:</strong></td>
                                                 <td>
                                                     <table class="table table-bordered">
@@ -148,8 +132,8 @@
                                                     </table>
                                                 </td>
                                             </tr>`;
-                        } else if (typeof value === 'object') {
-                            contentHTML += `<tr>
+                            } else if (typeof value === 'object') {
+                                contentHTML += `<tr>
                                                 <td><strong>${formatKeyName(key)}:</strong></td>
                                                 <td>
                                                     <table class="table table-bordered">
@@ -159,24 +143,24 @@
                                                     </table>
                                                 </td>
                                             </tr>`;
-                        } else {
-                            contentHTML += `<tr><td><strong>${formatKeyName(key)}:</strong></td><td>${value}</td></tr>`;
+                            } else {
+                                contentHTML += `<tr><td><strong>${formatKeyName(key)}:</strong></td><td>${value}</td></tr>`;
+                            }
                         }
-                    }
-                    contentHTML += '</table>';
-                    contentHTML += '</li>';
-                });
-                contentHTML += '</ul>';
-                return contentHTML;
-            } else {
-                return '<p>No threatened data available.</p>';
+                        contentHTML += '</table>';
+                        contentHTML += '</li>';
+                    });
+                    contentHTML += '</ul>';
+                    return contentHTML;
+                } else {
+                    return '<p>No threatened data available.</p>';
+                }
             }
-        }
 
-        function digitalUserRiskContent(dataContent) {
-            if (Array.isArray(dataContent) && dataContent.length > 0) {
-                const riskItem = dataContent[0]; // Assuming there is only one risk item in the array
-                return `<h4>Email At Risk Low</h4>
+            function digitalUserRiskContent(dataContent) {
+                if (Array.isArray(dataContent) && dataContent.length > 0) {
+                    const riskItem = dataContent[0]; // Assuming there is only one risk item in the array
+                    return `<h4>Email At Risk Low</h4>
                         ${generateEmailList(riskItem["email_at_risk_low"])}
                         <h4>Email At Risk Medium</h4>
                         ${generateEmailList(riskItem["email_at_risk_medium"])}
@@ -190,39 +174,39 @@
                         ${generateHackedEmailAddresses(riskItem["hacked_email_address"]["hacked_email_address"])}
                         <h4>Hacked Email Addresses Solution</h4>
                         <p>${riskItem["hacked_email_address"]["hacked_email_address_solution"]}</p>`;
-            } else {
-                return '<p>No Digital User Risk data available.</p>';
+                } else {
+                    return '<p>No Digital User Risk data available.</p>';
+                }
             }
-        }
 
-        function generateEmailList(emails) {
-            if (Array.isArray(emails) && emails.length > 0) {
-                let emailListHTML = '<ul>';
-                emails.forEach((email) => {
-                    emailListHTML += `<li>${email}</li>`;
-                });
-                emailListHTML += '</ul>';
-                return emailListHTML;
-            } else {
-                return '<p>No email data available.</p>';
+            function generateEmailList(emails) {
+                if (Array.isArray(emails) && emails.length > 0) {
+                    let emailListHTML = '<ul>';
+                    emails.forEach((email) => {
+                        emailListHTML += `<li>${email}</li>`;
+                    });
+                    emailListHTML += '</ul>';
+                    return emailListHTML;
+                } else {
+                    return '<p>No email data available.</p>';
+                }
             }
-        }
 
-        function generateHackedEmailAddresses(hackedEmails) {
-            if (Array.isArray(hackedEmails) && hackedEmails.length > 0) {
-                let hackedEmailsHTML = '<ul>';
-                hackedEmails.forEach((hackedEmail) => {
-                    hackedEmailsHTML += `<li>${hackedEmail}</li>`;
-                });
-                hackedEmailsHTML += '</ul>';
-                return hackedEmailsHTML;
-            } else {
-                return '<p>No hacked email addresses available.</p>';
+            function generateHackedEmailAddresses(hackedEmails) {
+                if (Array.isArray(hackedEmails) && hackedEmails.length > 0) {
+                    let hackedEmailsHTML = '<ul>';
+                    hackedEmails.forEach((hackedEmail) => {
+                        hackedEmailsHTML += `<li>${hackedEmail}</li>`;
+                    });
+                    hackedEmailsHTML += '</ul>';
+                    return hackedEmailsHTML;
+                } else {
+                    return '<p>No hacked email addresses available.</p>';
+                }
             }
-        }
 
-        function reportDetailContent(dataContent) {
-            return `<div>
+            function reportDetailContent(dataContent) {
+                return `<div>
                         <table class="table table-bordered">
                             <tr>
                                 <td><strong>Client ID:</strong></td>
@@ -283,10 +267,26 @@
                             <li><strong>Solution:</strong> ${dataContent["Vulnerability"][0]["solution"]}</li>
                         </ul>
                     </div>`;
+            }
+
+            showHome();
+            const showHomeButton = document.querySelector('.sidebar .home');
+            showHomeButton.onclick = showHome;
+            const sidebarButtons = document.querySelector('.sidebar .sidebar-buttons');
+            const mostRecentChildKeys = Object.keys(reportData);
+            mostRecentChildKeys.forEach(key => {
+                const button = document.createElement('button');
+                button.className = 'btn btn-primary btn-block';
+                button.innerText = formatKeyName(key);
+                button.onclick = (event) => showBasedOnChildClicked(event, key);
+                sidebarButtons.appendChild(button);
+            });
+            const signature = document.querySelector('.sidebar .signature');
+            signature.innerText = new Date().getFullYear().toString() + ' Nguyen Anh Tuan Le';
+        } else {
+            alert('No data found!');
         }
-    } else {
-        alert('No data found!');
-    }
+    });
 </script>
 </body>
 </html>
