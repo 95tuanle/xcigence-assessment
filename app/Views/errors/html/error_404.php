74506a27f300799880a8632165720726b9da198c
95tuanle <tlna95@icloud.com>
2023-10-30 13:47:03 -0400

    boilerplate

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -85,7 +85,7 @@
             <?= nl2br(esc($message)) ?>
         <?php else : ?>
             <?= lang('Errors.sorryCannotFind') ?>
-        <?php endif ?>
+        <?php endif; ?>
     </p>
 </div>
 </body>
